c77b4933add17207f77bf60e2ebc75714c637613
Ammon Murray <ammonmurray1@gmail.com>
2020-03-06 13:03:08 -0700

    fixed some css on the appointments page

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -1,47 +1,21 @@
-import React, {Component} from 'react';
-import "./Appointment.css"
+import React from "react";
+import "./Appointment.css";
 
+  function Appointment() {
+    return (
+      <div>
+        <iframe
+          title="appointment_form"
+          src="https://services.cognitoforms.com/f/bCHPQnDbSkCl5AXJv6bWMQ?id=1"
+          frameborder="0"
+          scrolling="yes"
+          seamless="seamless"
+          height="574"
+          width="100%"
+        ></iframe>
+        <script src="https://services.cognitoforms.com/scripts/embed.js"></script>
+      </div>
+    );
+  }
 
-
-class Appointment extends Component {
-    constructor(){
-        super()
-        this.state = {
-            date: new Date(),
-            inputDate: ''
-        }
-    }
-
-    onChange = date => this.setState({ date })
-
-    handleChange = (e) => {
-        this.setState({
-            inputDate: e.target.value
-        })
-    }
-    
-
-    submit = (e) => {
-        e.preventDefault()
-        console.log('input Date', typeof this.state.inputDate)
-        this.setState({
-            date: new Date(this.state.inputDate)
-        })
-    }
-
-   
-    render() {
-    return(
-        <div>
-            
-           
-            
-        
-            <iframe title='appointment_form' src="https://services.cognitoforms.com/f/bCHPQnDbSkCl5AXJv6bWMQ?id=1" frameborder="0" scrolling="yes" seamless="seamless" height="574" width="100%"></iframe>
-            <script src="https://services.cognitoforms.com/scripts/embed.js"></script>
-        </div>
-     );
-    }
-}
-
-export default Appointment
+export default Appointment;
